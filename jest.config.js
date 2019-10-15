22b345d50ecb9d3631ee9eb26cca1a70b6ede78c
Krzysztof Sakwerda <ksakwerda@gmail.com>
2019-10-15 11:41:20 +0200

    chore: no ~ imports in this package

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,7 @@ module.exports = {
   moduleNameMapper: {
     "assets/(.*).(jpe?g|png|gif|ico|svg)$":
       "<rootDir>/src/__mocks__/fileMock.js",
-    "^~/(.*)": "<rootDir>/$1",
     "\\.(css|less)$": "<rootDir>/src/__mocks__/styleMock.js"
-
   },
   testMatch: ["**/__tests__/**/*.test.ts?(x)"],
   transform: {
