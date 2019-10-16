ec8cbb50fb81cb6a68c3802df2edddfad61ae452
Krzysztof Sakwerda <ksakwerda@gmail.com>
2019-10-16 14:50:13 +0200

    fix: borders and selected focus styling

--- src/components/segmentedControl/button.tsx
+++ src/components/segmentedControl/button.tsx
@@ -22,23 +22,20 @@ export const Button: FC<Props> = ({
     <button
       type="submit"
       className={classnames(
-        "flex w-12/12 px-8 justify-center items-center text-white leading-xs transition-2 cursor-pointer font-bold text-base focus:outline-none",
+        "flex w-12/12 px-8 justify-center items-center text-white leading-xs transition-2 cursor-pointer font-bold text-base border-t-2 border-b-2 border-r-2 focus:outline-none",
         small ? "py-8" : "py-16",
-        selected
-          ? "bg-teal hover:bg-teal-dark focus:bg-teal"
-          : "bg-white border-teal text-teal",
+        selected ? "bg-teal" : "bg-white text-teal",
         disabled
           ? "cursor-not-allowed border-grey-3 bg-grey-1 text-grey-3 hover:border-grey-3 hover:bg-grey-1"
-          : null,
+          : "border-teal",
         {
           "rounded-none": position === "middle",
-          "rounded-l": position === "left",
+          "rounded-l border-l-2": position === "left",
           "rounded-r": position === "right",
-          "border-r-2": !selected || disabled,
-          "border-l-2": (!selected || disabled) && position === "left",
           "hover:bg-teal-light active:bg-teal-bright focus:shadow-segmentedControlFocus":
             !selected && !disabled,
-          "border-t-2 border-b-2": !selected || disabled
+          "hover:bg-teal-dark hover:border-teal-dark active:bg-teal-dark active:bg-teal-dark focus:bg-teal-dark focus:border-teal-dark":
+            selected && !disabled
         }
       )}
       onClick={onClick}
