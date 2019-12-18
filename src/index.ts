4d4ba6dec558000e2c62ef89077fe858a54c62bb
Christian Felder (masone) <ema@rh-productions.ch>
2019-12-18 15:31:19 +0100

    export from

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
-import {
-  withDefaultConfig,
-  defaultConfig,
-  getComponentPaths
-} from "./tailwind/index"
+// import {
+//   withDefaultConfig,
+//   defaultConfig,
+//   getComponentPaths
+// } from "./tailwind/index"
 
-import Button from "./components/button"
-import Pagination from "./components/pagination"
-import Dropdown from "./components/dropdown/index"
-import DropdownWithAutosuggest from "./components/dropdown/withAutosuggest"
-import SegmentedControl from "./components/segmentedControl/index"
+export { Button } from "./components/button"
+import { Pagination } from "./components/pagination"
+import { Dropdown } from "./components/dropdown/index"
+import { DropdownWithAutosuggest } from "./components/dropdown/withAutosuggest"
+import { SegmentedControl } from "./components/segmentedControl/index"
 
-export {
-  Button,
-  Pagination,
-  Dropdown,
-  DropdownWithAutosuggest,
-  SegmentedControl
-}
-
-export { withDefaultConfig, defaultConfig, getComponentPaths }
+// export { withDefaultConfig, defaultConfig, getComponentPaths }
