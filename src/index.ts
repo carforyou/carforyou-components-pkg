10081b3dfd8c8ad3277bcbfe9798b345f357ff66
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-01-29 16:08:15 +0100

    fix: it needs to be index

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import Dropdown from "./components/dropdown/index"
 import DropdownWithAutosuggest from "./components/dropdown/withAutosuggest"
 import SegmentedControl from "./components/segmentedControl/index"
 import Spinner from "./components/spinner"
-import Intercom from "./components/intercom"
+import Intercom from "./components/intercom/index"
 
 export {
   Button,
