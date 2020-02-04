87284cf53da76d489e0d3737c4d23e4b973f9525
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-02-04 13:35:38 +0100

    fix: switch the button colour to match intercom configuration

--- src/components/intercom/index.tsx
+++ src/components/intercom/index.tsx
@@ -121,7 +121,7 @@ const Intercom: FC<Props> = ({
       <div
         className={classNames(
           "intercom-launcher text-white py-2 fixed cursor-pointer transition-3 hover:opacity-60",
-          state === State.Open ? "bg-intercom" : "bg-grey-4"
+          state === State.Open ? "bg-salmon" : "bg-grey-4"
         )}
         id={intercomLauncherId}
         onClick={() => {
