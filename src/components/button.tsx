9ec12693fd253c21ad2aea1b02750d74f57ebf40
GutierrezGo <virginia@carforyou.ch>
2020-02-05 13:15:53 +0100

    added wrapper element, deleted paddings

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -27,37 +27,39 @@ export const Button: FC<Props> = ({
   submit,
   icon
 }) => {
-  const padding = classnames("px-10 py-5", small ? "min-h-36" : "min-h-52")
+  const padding = classnames("px-10", small ? "min-h-36" : "min-h-52")
   const { clonedElement, isWrapped } = wrapLink(children, padding)
 
   return (
-    <button
-      type={submit ? "submit" : "button"}
-      className={classnames(
-        "flex w-12/12 justify-center items-center text-white leading-xs transition-2 cursor-pointer font-bold text-base rounded border focus:outline-none",
-        { [padding]: !isWrapped },
-        teal
-          ? "bg-teal border-teal hover:bg-teal-dark focus:bg-teal"
-          : tealBorder
-          ? "bg-white border-teal text-teal hover:opacity-60"
-          : "bg-salmon border-salmon hover:bg-salmon-dark focus:bg-salmon",
-        disabled
-          ? "bg-grey-3 border-grey-3 cursor-not-allowed hover:bg-grey-3"
-          : null
-      )}
-      onClick={onClick}
-      disabled={disabled}
-      data-testid={dataTestid}
-    >
-      {icon ? (
-        <>
-          {icon()}
-          <span className="pl-5 pr-8">{clonedElement}</span>
-        </>
-      ) : (
-        clonedElement
-      )}
-    </button>
+    <div className="flex flex-col">
+      <button
+        type={submit ? "submit" : "button"}
+        className={classnames(
+          "flex w-12/12 justify-center items-center text-white leading-xs transition-2 cursor-pointer font-bold text-base rounded border focus:outline-none",
+          { [padding]: !isWrapped },
+          teal
+            ? "bg-teal border-teal hover:bg-teal-dark focus:bg-teal"
+            : tealBorder
+            ? "bg-white border-teal text-teal hover:opacity-60"
+            : "bg-salmon border-salmon hover:bg-salmon-dark focus:bg-salmon",
+          disabled
+            ? "bg-grey-3 border-grey-3 cursor-not-allowed hover:bg-grey-3"
+            : null
+        )}
+        onClick={onClick}
+        disabled={disabled}
+        data-testid={dataTestid}
+      >
+        {icon ? (
+          <>
+            {icon()}
+            <span className="pl-5 pr-8">{clonedElement}</span>
+          </>
+        ) : (
+          clonedElement
+        )}
+      </button>
+    </div>
   )
 }
 
