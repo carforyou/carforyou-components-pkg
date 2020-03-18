7d1b15606b991d3adccfc654f32e9630b35e4d1e
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-03-18 09:20:04 +0100

    fix: correct error handling

--- src/components/fieldHelpers/withValidationError.tsx
+++ src/components/fieldHelpers/withValidationError.tsx
@@ -5,20 +5,22 @@ interface Props {
   children: (error: boolean) => ReactChild
 }
 
-const WithValidationError: FC<Props> = ({ errors, children }) => (
-  <>
-    {children(!!errors)}
-    <div className="text-sm text-salmon h-19">
-      {errors.length === 1 && errors[0]}
-      {errors.length > 1 ? (
-        <ul>
-          {errors.map((message, index) => (
-            <li key={`error-${index}`}>{message}</li>
-          ))}
-        </ul>
-      ) : null}
-    </div>
-  </>
-)
+const WithValidationError: FC<Props> = ({ errors, children }) => {
+  return (
+    <>
+      {children(!!errors && errors.length > 0)}
+      <div className="text-sm text-salmon h-19">
+        {errors.length === 1 && errors[0]}
+        {errors.length > 1 ? (
+          <ul>
+            {errors.map((message, index) => (
+              <li key={`error-${index}`}>{message}</li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+    </>
+  )
+}
 
 export default WithValidationError
