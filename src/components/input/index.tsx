a77f846eef4fd15424e99e929c9d57750fe6a6f7
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-03-19 08:39:57 +0100

    chore: extract input field component

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,9 +1,10 @@
 import React, { FocusEvent, ReactElement, RefObject } from "react"
 import classNames from "classnames"
 
-import WithValidationError from "./fieldHelpers/withValidationError"
-import WithLabel from "./fieldHelpers/withLabel"
-import WithClearButton from "./fieldHelpers/withClearButton"
+import WithValidationError from "../fieldHelpers/withValidationError"
+import WithLabel from "../fieldHelpers/withLabel"
+import WithClearButton from "../fieldHelpers/withClearButton"
+import InputField from "./inputField"
 
 interface Props {
   name: string
@@ -20,48 +21,6 @@ interface Props {
   onChange: (e: { target: { value: string | number } }) => void
   onBlur: (e: FocusEvent<any>) => void
   ref?: RefObject<HTMLInputElement>
-}
-
-const validateNumber = e => {
-  const key = e.which || e.keyCode
-
-  if (
-    (!e.shiftKey &&
-      !e.altKey &&
-      !e.ctrlKey &&
-      // numbers
-      key >= 48 &&
-      key <= 57) ||
-    // Numeric keypad
-    (key >= 96 && key <= 105) ||
-    // Backspace and Tab and Enter
-    key === 8 ||
-    key === 9 ||
-    key === 13 ||
-    // Home and End
-    key === 35 ||
-    key === 36 ||
-    // left and right arrows
-    key === 37 ||
-    key === 39 ||
-    // Del and Ins
-    key === 46 ||
-    key === 45
-  ) {
-    return
-  }
-
-  e.preventDefault()
-}
-
-const validateDecimal = e => {
-  const key = e.which || e.keyCode
-
-  // Period (decimal)
-  if (key === 190 && !e.target.value.includes(".")) {
-    return
-  }
-  validateNumber(e)
 }
 
 function Input({
@@ -81,30 +40,20 @@ function Input({
   onBlur
 }: Props): ReactElement {
   const renderInput = hasError => (
-    <input
+    <InputField
       ref={ref}
-      id={name}
       name={name}
-      type="text"
       value={value || ""}
       placeholder={placeholder || ""}
       className={classNames("w-12/12", {
         "input--withClearButton": hasClearButton
       })}
-      inputMode={mode !== "text" ? mode : null}
-      onKeyDown={
-        mode === "numeric"
-          ? validateNumber
-          : mode === "decimal"
-          ? validateDecimal
-          : null
-      }
-      onChange={onChange}
-      onBlur={onBlur}
-      data-testid={name}
-      data-valid={!hasError}
+      mode={mode}
+      hasError={hasError}
       disabled={disabled}
       required={required}
+      onChange={onChange}
+      onBlur={onBlur}
     />
   )
 
