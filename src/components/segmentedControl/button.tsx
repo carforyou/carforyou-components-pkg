67b11bb23622aaf620668a3622d92a74a87cad9a
Krzysztof Sakwerda <krzysztof@carforyou.ch>
2020-04-14 11:31:21 +0200

    fix: links in buttons in ie11 (#170)

--- src/components/segmentedControl/button.tsx
+++ src/components/segmentedControl/button.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from "react"
+import React, { FC, ReactNode, createElement } from "react"
 import classnames from "classnames"
 
 import { wrapLink } from "../../lib/buttonHelper"
@@ -23,31 +23,28 @@ export const Button: FC<Props> = ({
   const padding = classnames("px-8", small ? "py-8" : "py-16")
   const { clonedElement, isWrapped } = wrapLink(children, padding)
 
-  return (
-    <button
-      type="button"
-      className={classnames(
-        "flex w-12/12 justify-center items-center leading-xs transition duration-200 cursor-pointer font-bold text-base border-t-2 border-b-2 border-r-2 focus:outline-none",
-        selected ? "bg-teal text-white" : "bg-transparent text-teal",
-        disabled
-          ? "cursor-not-allowed border-grey-3 bg-grey-1 text-grey-3 hover:border-grey-3 hover:bg-grey-1"
-          : "border-teal focus:shadow-focus",
-        {
-          [padding]: !isWrapped,
-          "rounded-none": position === "middle",
-          "rounded-l border-l-2": position === "left",
-          "rounded-r": position === "right",
-          "hover:bg-teal-light active:bg-teal-bright": !selected && !disabled,
-          "hover:bg-teal-dark hover:border-teal-dark active:bg-teal-dark active:bg-teal-dark focus:bg-teal-dark focus:border-teal-dark":
-            selected && !disabled
-        }
-      )}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      {clonedElement}
-    </button>
-  )
+  return createElement(isWrapped ? "div" : "button", {
+    type: "button",
+    className: classnames(
+      "flex w-12/12 justify-center items-center leading-xs transition duration-200 cursor-pointer font-bold text-base border-t-2 border-b-2 border-r-2 focus:outline-none",
+      selected ? "bg-teal text-white" : "bg-transparent text-teal",
+      disabled
+        ? "cursor-not-allowed border-grey-3 bg-grey-1 text-grey-3 hover:border-grey-3 hover:bg-grey-1"
+        : "border-teal focus:shadow-focus",
+      {
+        [padding]: !isWrapped,
+        "rounded-none": position === "middle",
+        "rounded-l border-l-2": position === "left",
+        "rounded-r": position === "right",
+        "hover:bg-teal-light active:bg-teal-bright": !selected && !disabled,
+        "hover:bg-teal-dark hover:border-teal-dark active:bg-teal-dark active:bg-teal-dark focus:bg-teal-dark focus:border-teal-dark":
+          selected && !disabled
+      }
+    ),
+    onClick: !disabled ? onClick : null,
+    disabled,
+    children: clonedElement
+  })
 }
 
 export default Button
