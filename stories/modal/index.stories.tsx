c9d4f45442d1ce4602f854ec21fab90f62c8e3f6
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-04-21 11:08:17 +0200

    feat: support dark modals

---
+++ stories/modal/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import { storiesOf } from "@storybook/react"
+
+import generateStory from "./generator"
+
+storiesOf("Modal", module)
+  .add("Small", generateStory({ size: "small" }))
+  .add("Medium", generateStory({ size: "medium" }))
+  .add("Large", generateStory({ size: "large" }))
+  .add("Fulscreen", generateStory({ size: "fullscreen" }))
+  .add("Dark Fulscreen", generateStory({ style: "dark", size: "fullscreen" }))
