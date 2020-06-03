4d37aca2a1a6dd7e06bd6bd170a308586f0eb5aa
ufkaya <40758856+ufkaya@users.noreply.github.com>
2020-06-03 17:57:57 +0200

    fix: customValue (#196)
    * fix: check if value null

--- src/components/dropdown/menu.tsx
+++ src/components/dropdown/menu.tsx
@@ -34,7 +34,7 @@ interface Props<T> {
 }
 
 const isCustomValue = (value): boolean => {
-  if (typeof value === "object") {
+  if (value && typeof value === "object") {
     return "customValue" in value
   }
   return false
