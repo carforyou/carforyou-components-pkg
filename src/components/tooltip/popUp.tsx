acd9bbcd2a3364655409a872b467c3f591ddefa8
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-07-13 09:46:32 +0200

    fix: use full class names instead of dynamic classes (purgecss)

--- src/components/tooltip/popUp.tsx
+++ src/components/tooltip/popUp.tsx
@@ -5,23 +5,17 @@ interface Props {
   position: "top" | "bottom" | "left" | "right"
 }
 
-const tooltipPosition = {
-  top: "bottom",
-  bottom: "top",
-  left: "left",
-  right: "right",
-}
-
 const PopUp: FC<Props> = ({ children, position }) => (
   <div
     className={classNames(
       "w-tooltip p-15 bg-white rounded-4 shadow-hard z-modal absolute transform",
-      `tooltip_${position}`,
-      `${tooltipPosition[position]}-tooltip`,
-      `m${tooltipPosition[position][0]}-10`,
       {
         "left-half -translate-x-1/2": ["top", "bottom"].includes(position),
         "top-half -translate-y-1/2": ["left", "right"].includes(position),
+        "tooltip_top bottom-tooltip mb-10": position === "top",
+        "tooltip_bottom top-tooltip mt-10": position === "bottom",
+        "tooltip_left left-tooltip ml-10": position === "left",
+        "tooltip_right right-tooltip mr-10": position === "right",
       }
     )}
   >
