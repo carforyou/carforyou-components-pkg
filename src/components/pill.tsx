3743ab16a9a175df475f156f51f011df5d398f9a
Christian Felder (masone) <ema@rh-productions.ch>
2020-07-14 19:49:26 +0200

    feat: add pill component

---
+++ src/components/pill.tsx
@@ -0,0 +1,20 @@
+import React, { FC, ReactNode } from "react"
+import classnames from "classnames"
+
+interface Props {
+  children: ReactNode
+  secondary: boolean
+}
+
+export const Pill: FC<Props> = ({ children, secondary }) => (
+  <div
+    className={classnames(
+      "inline-block text-white rounded-pill text-xs pt-0 pb-1 px-5",
+      secondary ? "bg-black" : "bg-salmon"
+    )}
+  >
+    {children}
+  </div>
+)
+
+export default Pill
