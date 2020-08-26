2d8ba8325d8abb9db6c9c2b229407734371e5dc5
Christian Felder (masone) <ema@rh-productions.ch>
2020-08-26 16:45:16 +0200

    configure webpack to load css modules

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 
 module.exports = ({ config }) => {
+  config.resolve.extensions.push(".ts", ".tsx")
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
@@ -12,7 +13,25 @@ module.exports = ({ config }) => {
       }
     ]
   })
-  config.resolve.extensions.push(".ts", ".tsx")
+
+  // Exclude modules from storybooks default CSS loader and inject our own
+  // https://github.com/storybookjs/storybook/issues/6055#issuecomment-521046352
+  config.module.rules.find(
+    rule => rule.test.toString() === '/\\.css$/',
+  ).exclude = /\.module\.css$/;
+
+  config.module.rules.push({
+    test: /\.module\.css$/,
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+        },
+      },
+    ],
+  });
 
   // Storybook does not yet allow customizing the postcss config, so we push our own
   // https://github.com/storybooks/storybook/blob/a52efc786172527b51b455f82cc5a7ed73246385/lib/core/src/server/preview/base-webpack.config.js
