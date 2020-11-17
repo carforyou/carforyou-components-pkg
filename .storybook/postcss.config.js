3562eb1325dcdcb6400c52fb55da88d2a1ba90fc
Marine-Berthier <marine@DESKTOP-EVDLM3C.localdomain>
2020-11-17 11:20:56 +0100

    uninstall storybook

--- .storybook/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const tailwindConfig = require("./tailwind/defaultConfig").default
-
-module.exports = {
-  plugins: [
-    require("postcss-easy-import"),
-    require("tailwindcss")(tailwindConfig),
-    require("autoprefixer")
-  ]
-}
