a0434f3fe172d868da0956049b02464f1a0ce1ea
Marine-Berthier <marine@DESKTOP-EVDLM3C.localdomain>
2020-11-24 14:28:45 +0100

    set up dropdown

---
+++ src/stories/dropdown.stories.js
@@ -0,0 +1,36 @@
+/*import React from "react"
+import Dropdown from "../components/dropdown/index"
+import { action } from "@storybook/addon-actions"
+
+export default {
+  title: "Dropdown",
+  component: Dropdown,
+}
+
+const Template = (args) => {
+  return (
+    <div className="mx-30 mb-40 w-12/12 md:w-3/12">
+      <div className="relative">
+        <Dropdown
+          options={options()}
+          onSelect={onSelect()}
+          toggle={({ name, placeholder }) => (
+            <div className="text-left">
+              <span className={placeholder ? "font-regular" : "font-bold"}>
+                {name}
+              </span>
+            </div>
+          )}
+          selected={selected()}
+          disabled={disabled()}
+          placeholder={placeholderKnob()}
+        />
+      </div>
+    </div>
+  )
+}
+
+export const WithPlaceholder = Template.bind({})
+WithPlaceholder.args = {
+  
+}*/
