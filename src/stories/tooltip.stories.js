94428df4afe362f4e5b90309cc020e8454e401e6
Marine-Berthier <marine@DESKTOP-EVDLM3C.localdomain>
2020-11-30 09:19:06 +0100

    chores: stories for tooltip wip

---
+++ src/stories/tooltip.stories.js
@@ -0,0 +1,56 @@
+import React from "react"
+import Tooltip from "../components/tooltip/index"
+import { TooltipPosition, TooltipAlignment } from "../components/tooltip/index"
+
+export default {
+  title: "Tooltip",
+  component: Tooltip,
+  args: {
+    storyName: "Default",
+  },
+  argTypes: {
+    buttonName: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+const Template = ({ args }) => {
+  return (
+    <div className="mx-30 mb-40">
+      <div className="text-2xl mb-20">Example</div>
+      <div className="w-12/12 flex justify-center">
+        <Tooltip {...args}>
+          <span>I have a tooltip</span>
+        </Tooltip>
+      </div>
+    </div>
+  )
+}
+
+const story = ({ position = "top", alignment = "center" }) => {
+  Default.args = {
+    renderContent: () => (
+      <p className="w-modalSmall">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna
+        augue, consectetur at sapien eu, facilisis gravida.
+      </p>
+    ),
+    preferredPosition: { position },
+    alignment: { alignment },
+  }
+}
+
+Object.values(TooltipPosition).reduce(
+  (accPosition, position) =>
+    Object.values(TooltipAlignment).reduce(
+      (accAlignment, alignment) =>
+        accAlignment.add(
+          `On the ${position} aligned to ${alignment}`,story({ position, alignment })
+          ),
+      accPosition
+    ),
+  (export const Default = Template.bind({}))
+)
