dbe3ebd6436fba3847e9ad0782bb27c563af279e
Marine-Berthier <marine@DESKTOP-EVDLM3C.localdomain>
2020-12-02 13:12:40 +0100

    chore: migrate checkbox wip

--- src/stories/radioButton/selected.stories.js
+++ src/stories/radioButton/selected.stories.js
@@ -2,12 +2,15 @@ import React, { useState } from "react"
 import RadioButton from "../../components/radioButton"
 import { action } from "@storybook/addon-actions"
 
+const onChange = () => action("onChange")
+const initialValue = "A"
 
 export default {
   title: "Radio Button/Selected",
   component: RadioButton,
   args: {
     storyName: "",
+    value: initialValue,
   },
   argTypes: {
     storyName: {
@@ -19,19 +22,20 @@ export default {
 }
 
 const Template = (args) => {
-  const [value, setValue] = useState(true)
+  const [value, setValue] = useState()
   return (
     <div className="mx-30 mb-40">
-      <div className="text-2xl mb-20">Example</div>
+      <div className="text-2xl mb-20">{args.storyName}</div>
       <div className="w-12/12 md:w-3/12">
-        <Checkbox
-          {...args}
+        <RadioButton
           checked={value}
+          name={name}
           onChange={(e) => {
-            setValue(e.target.checked)
+            setValue(e.target.value)
             onChange()(e)
           }}
           renderLabel={args.label ? () => args.label : null}
+          {...args}
         />
       </div>
     </div>
