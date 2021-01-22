78c42458c9ce8a28d4ef91d5b47e25414913c4a4
Marine-Berthier <52455155+Marine-Berthier@users.noreply.github.com>
2021-01-22 10:21:24 +0100

    feat: add new component previewCard
    feat: add new component previewCard

---
+++ src/stories/previewCard.stories.js
@@ -0,0 +1,42 @@
+import React from "react"
+
+import StoryContainer from "./storyContainer"
+import PreviewCard from "../components/previewCard"
+
+export default {
+  title: "Preview card",
+  component: PreviewCard,
+  args: {
+    storyName: "Default",
+    previewLabel: "Show more",
+    footerTitle: "Dealer Name",
+    footerText: "Zürcherstrasse, 8953 Dietikon",
+    cardTitle: "Sportgarage",
+    image:
+      "https://images.preprod.carforyou.ch/2020/12/04/13/45/32/2-promotionimage-1316-vt5CK5OFsSY7.jpg",
+    logo:
+      "https://images.preprod.carforyou.ch/2020/12/07/11/52/50/2-logo-1316-Cw3Xsal82n5X.png",
+    link: "#",
+  },
+  argTypes: {
+    storyName: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+const Template = (args) => {
+  return (
+    <StoryContainer
+      title={args.storyName}
+      component={<PreviewCard {...args} />}
+    />
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  storyName: "Default",
+}
