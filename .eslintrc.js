0db18e0b7dd8569be541149e12dc2ae73129777f
talamcol <71456764+talamcol@users.noreply.github.com>
2021-02-09 13:44:10 +0100

    fix: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ["@carforyou/eslint-config/react"],
+  overrides: [
+    {
+      files: ["*.config.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+        "import/no-internal-modules": "off",
+      },
+    },
+  ],
 }
