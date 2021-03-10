1a302cad5a1012717de95b25872a7f4bb2cdae63
Aleksandar Popovic <acapop77@gmail.com>
2021-03-10 20:52:37 +0100

    fix: add more descriptive size

--- src/stories/collapsible.stories.tsx
+++ src/stories/collapsible.stories.tsx
@@ -62,7 +62,7 @@ MobileChevron.args = {
   ...Default.args,
   renderToggle: () => (
     <div className="flex w-12/12 border-2 px-10 py-30">
-      Observe chevron position on smaller screen vs big screen
+      Observe chevron position on smaller screen vs big lg: screen
     </div>
   ),
   mobileChevron: true,
