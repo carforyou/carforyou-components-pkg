75529492bb0eaa6044f84835757490dd95a82540
ufkaya <ufuk.kaya@autoricardo.ch>
2021-05-03 13:09:11 +0200

    feat: add tailwindcss compat

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -33,22 +33,5 @@ module.exports = ({ config }) => {
     ],
   });
 
-  // Storybook does not yet allow customizing the postcss config, so we push our own
-  // https://github.com/storybooks/storybook/blob/a52efc786172527b51b455f82cc5a7ed73246385/lib/core/src/server/preview/base-webpack.config.js
-  // config.module.rules.push({
-  //   include: path.resolve(__dirname, "../"),
-  //   test: /\.css$/,
-  //   use: [
-  //     {
-  //       loader: "postcss-loader",
-  //       options: {
-  //         config: {
-  //           path: ".storybook/",
-  //         },
-  //       },
-  //     },
-  //   ],
-  // })
-
   return config
 }
