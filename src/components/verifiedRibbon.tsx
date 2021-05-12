02ad02113252feb66cb1c957935560503e39a8bb
Marko Reljin <reljinmarko@gmail.com>
2021-05-12 16:10:27 +0200

    add verified ribbon

---
+++ src/components/verifiedRibbon.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from "react"
+
+import Verified from "./icons/verified"
+import Ribbon from "./icons/ribbon"
+
+interface Props {
+  label: string
+}
+
+const VerifiedRibbon: FC<Props> = ({ label }) => {
+  return (
+    <>
+      <span className="text-grey-dark">
+        <Ribbon />
+      </span>
+      <span className="absolute top-10 left-0 ml-10">
+        <Verified />
+        <span className="text-white font-bold pl-3 text-sm">{label}</span>
+      </span>
+    </>
+  )
+}
+
+export default VerifiedRibbon
