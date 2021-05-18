761f0a7344834d476f17089a996c238597da755f
Marko Reljin <reljinmarko@gmail.com>
2021-05-18 16:10:35 +0200

    feat: added storybook for component and added fontSize param

--- src/components/verifiedRibbon.tsx
+++ src/components/verifiedRibbon.tsx
@@ -1,15 +1,30 @@
 import React, { FC } from "react"
 
+import classnames from "classnames"
+
 import Verified from "./icons/verified"
 import Ribbon from "./icons/ribbon"
 
 interface Props {
-  label: string
+  label?: string
   ribbonWidth: string
   iconWidth: string
+  fontSize?:
+    | "text-xs"
+    | "text-sm"
+    | "text-base"
+    | "text-lg"
+    | "text-xl"
+    | "text-2xl"
+    | "text-3xl"
 }
 
-const VerifiedRibbon: FC<Props> = ({ label, ribbonWidth, iconWidth }) => {
+const VerifiedRibbon: FC<Props> = ({
+  label,
+  ribbonWidth,
+  iconWidth,
+  fontSize = "text-base",
+}) => {
   return (
     <div className="relative">
       <span className="text-grey-dark">
@@ -17,10 +32,13 @@ const VerifiedRibbon: FC<Props> = ({ label, ribbonWidth, iconWidth }) => {
       </span>
       <span className="absolute left-0 mt-10 ml-10">
         <Verified width={iconWidth} />
-        <span className="text-white font-bold pl-5">{label}</span>
+        <span className={classnames("text-white font-bold pl-5", fontSize)}>
+          {label}
+        </span>
       </span>
     </div>
   )
 }
 
 export default VerifiedRibbon
+export { Props as VerifiedRibbonProps }
