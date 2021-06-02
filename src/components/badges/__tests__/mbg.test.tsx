9832ffd0508377aafcdf6d8b14471ed89d80f384
Lorenzo Kappeler <me@lnk.codes>
2021-06-02 13:08:47 +0200

    test: mbgBadge

--- src/components/badges/__tests__/mbg.test.tsx
+++ src/components/badges/__tests__/mbg.test.tsx
@@ -1,17 +1,41 @@
-// import React from "react"
-// import { render, fireEvent } from "@testing-library/react"
+import React from "react"
+import { fireEvent, render } from "@testing-library/react"
 
-// import MbgBadge from "../mbg"
+import MbgBadge from "../mbg"
+import { DummyTooltip } from "../../../stories/badges/dummyTooltip"
 
-// describe("<MbgBadge>", () => {
-//   it("renders tooltip on hover", async () => {
-//     // Temporarly disable this test
-//     return
+describe("<MbgBadge>", () => {
+  it("renders the badge in english", () => {
+    const { getByText } = render(<MbgBadge language="en" />)
+    expect(getByText("Money-Back-Guarantee"))
+  })
 
-//     const { getByText, findByText } = render(<MbgBadge language="en" hasText />)
+  it("renders the badge without text", () => {
+    const { queryAllByText } = render(<MbgBadge language="en" size="small" />)
+    expect(queryAllByText("Money-Back-Guarantee")).toHaveLength(0)
+  })
 
-//     fireEvent.mouseEnter(getByText("moneyBackGuarantee.title"))
+  it("renders the badge in german", () => {
+    const { getByText } = render(<MbgBadge language="de" />)
+    expect(getByText("Geld-Zurück-Garantie"))
+  })
 
-//     expect(await findByText("moneybackTooltip.description"))
-//   })
-// })
+  it("renders the badge in french", () => {
+    const { getByText } = render(<MbgBadge language="fr" />)
+    expect(getByText("Garantie de remboursement"))
+  })
+
+  it("renders the badge in italian", () => {
+    const { getByText } = render(<MbgBadge language="it" />)
+    expect(getByText("Garanzia di rimborso"))
+  })
+
+  it("renders a tooltip on hover", async () => {
+    const { findByText, getByText } = render(
+      <MbgBadge language="en" tooltipContent={<DummyTooltip />} />
+    )
+
+    fireEvent.mouseEnter(getByText("Money-Back-Guarantee"))
+    expect(await findByText("Dummy tooltip"))
+  })
+})
