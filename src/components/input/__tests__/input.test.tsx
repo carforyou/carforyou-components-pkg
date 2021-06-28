025e194d2147089ae530251e2eaf4816a39e5fec
ghelshani <gzim.helshanii@gmail.com>
2021-06-28 14:05:00 +0200

    fix(debounce): tests

---
+++ src/components/input/__tests__/input.test.tsx
@@ -0,0 +1,94 @@
+import React from "react"
+
+import { fireEvent, render } from "@testing-library/react"
+
+import Input from "../index"
+
+describe("<Input>", () => {
+  describe("without debounce", () => {
+    it("executes the callback function", () => {
+      const onChangeMock = jest.fn()
+      const { getByPlaceholderText } = render(
+        <Input
+          name="testInput"
+          placeholder="testInput"
+          value="initial value"
+          mode="text"
+          onChange={onChangeMock}
+          hasClearButton
+        />
+      )
+      const input = getByPlaceholderText("testInput")
+      fireEvent.change(input, { target: { value: "Test" } })
+
+      expect(onChangeMock).toBeCalled()
+    })
+
+    it("clears the input", () => {
+      const onChangeMock = jest.fn()
+      const { getByTestId } = render(
+        <Input
+          name="testInput"
+          placeholder="testInput"
+          value="initial value"
+          mode="text"
+          onChange={onChangeMock}
+          hasClearButton
+        />
+      )
+      const clearButton = getByTestId("clearButton")
+      fireEvent.click(clearButton)
+
+      expect(onChangeMock).toBeCalled()
+    })
+  })
+
+  describe("with debounce", () => {
+    beforeEach(() => {
+      jest.useFakeTimers()
+    })
+
+    afterEach(() => {
+      jest.clearAllTimers()
+      jest.useRealTimers()
+    })
+
+    it("executes the callback function with a delay", () => {
+      const onChangeMock = jest.fn()
+      const { getByPlaceholderText } = render(
+        <Input
+          name="testInput"
+          placeholder="testInput"
+          value="initial value"
+          mode="text"
+          onChange={onChangeMock}
+          hasClearButton
+          debounce={200}
+        />
+      )
+      const input = getByPlaceholderText("testInput")
+      fireEvent.change(input, { target: { value: "Test" } })
+      jest.runAllTimers()
+
+      expect(onChangeMock).toBeCalled()
+    })
+
+    it("clears the input immediately", () => {
+      const onChangeMock = jest.fn()
+      const { getByTestId } = render(
+        <Input
+          name="testInput"
+          placeholder="testInput"
+          value="initial value"
+          mode="text"
+          onChange={onChangeMock}
+          hasClearButton
+        />
+      )
+      const clearButton = getByTestId("clearButton")
+      fireEvent.click(clearButton)
+
+      expect(onChangeMock).toBeCalled()
+    })
+  })
+})
