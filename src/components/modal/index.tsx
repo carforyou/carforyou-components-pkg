505d4fa098c520771c1004298c209f1c73876b81
ghelshani <gzim.helshanii@gmail.com>
2021-07-16 13:00:54 +0200

    feat(modal): update hook dependencies

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useEffect } from "react"
+import React, { FC, ReactNode, useCallback, useEffect } from "react"
 import classNames from "classnames"
 
 import Overlay from "./overlay"
@@ -35,22 +35,25 @@ const Modal: FC<ModalProps> = ({
   const overflowClass = verticalOverflow
     ? `overflow-y-${verticalOverflow}`
     : "md:overflow-y-auto overflow-y-scroll"
-  const handleClose = () => {
+  const handleClose = useCallback(() => {
     onClose ? onClose() : null
     close()
-  }
+  }, [onClose, close])
 
-  const handleKeys = (e) => {
-    if (e.keyCode === 27) {
-      e.preventDefault()
-      handleClose()
-    }
-  }
+  const handleKeys = useCallback(
+    (e) => {
+      if (e.keyCode === 27) {
+        e.preventDefault()
+        handleClose()
+      }
+    },
+    [handleClose]
+  )
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeys)
     return () => document.removeEventListener("keydown", handleKeys)
-  }, [])
+  }, [handleKeys])
 
   return (
     <div className="fixed inset-0 scrolling-touch overflow-y-scroll md:overflow-y-auto z-modal min-h-full transition duration-200 flex flex-col justify-center items-center">
