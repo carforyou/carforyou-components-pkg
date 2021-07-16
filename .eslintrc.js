309eb2e8052822bed9adb0305065a3fc3b7b576b
ghelshani <gzim.helshanii@gmail.com>
2021-07-16 12:54:04 +0200

    feat(input): add missing dependency on useEffect

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,23 @@
 module.exports = {
-  extends: ["@carforyou/eslint-config/react"],
+  extends: ["@carforyou/eslint-config/react", "plugin:react-hooks/recommended"],
   overrides: [
     {
       files: ["*.config.js"],
       rules: {
         "@typescript-eslint/no-var-requires": "off",
         "import/no-internal-modules": "off",
+      },
+    },
+    {
+      files: ["*"],
+      rules: {
+        "no-restricted-imports": "off",
+        "react-hooks/exhaustive-deps": [
+          "error",
+          {
+            additionalHooks: "useDeepCompareEffect",
+          },
+        ],
       },
     },
   ],
