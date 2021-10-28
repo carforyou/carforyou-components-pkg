8a86fbe70a273c3de80f64f98363d2a3e7ecbe3f
Marine-Berthier <marine@DESKTOP-EVDLM3C.localdomain>
2021-10-28 07:53:50 +0200

    feat: small version InvertedVerifiedBadge

--- src/components/badges/invertedVerified.tsx
+++ src/components/badges/invertedVerified.tsx
@@ -20,7 +20,7 @@ export const InvertedVerifiedBadge: FC<BadgeProps> = ({
     <BaseBadge
       icon={<VerifiedIcon width="24" height="24" />}
       tooltipContent={tooltipContent}
-      size="large"
+      size="small"
       withText={size === "large"}
       text={title[language]}
       background="grey"
