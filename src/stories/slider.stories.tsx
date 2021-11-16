1bcdaba87481eff1428fa32ea2871ab9be90f17e
Marine-Berthier <52455155+Marine-Berthier@users.noreply.github.com>
2021-11-16 11:03:51 +0100

    feat: add new component slider
    feat: add new component slider

---
+++ src/stories/slider.stories.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from "react"
+
+import { action } from "@storybook/addon-actions"
+
+import { StoryProps } from "./storyProps"
+import StoryContainer from "./storyContainer"
+import Slider, { SliderProps } from "../components/slider"
+
+const onChange = () => action("onChange")
+
+export default {
+  title: "Slider",
+  component: Slider,
+  args: {
+    storyName: "",
+    step: 50,
+    min: 0,
+    max: 1000,
+    name: "slider",
+    defaultValue: 20,
+    inputLabel: "Slider example",
+    handleChange: onChange(),
+  },
+  argTypes: {
+    storyName: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+interface Props extends StoryProps<unknown>, SliderProps {}
+
+const Template: FC<Props> = (args) => {
+  return (
+    <StoryContainer title={args.storyName} component={<Slider {...args} />} />
+  )
+}
+
+export const Default = Template.bind({})
+
+export const Required = Template.bind({})
+Required.args = {
+  required: true,
+}
+
+export const Error = Template.bind({})
+Error.args = {
+  error: "Error message",
+}
