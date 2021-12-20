d57261ca4fee164d7ae8b5e19aff3fda3b7ec817
Shpat Hyseni <shpat@centralsoft.io>
2021-12-20 15:33:14 +0100

    fix: signup color change

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -85,7 +85,7 @@ export const Button: FC<Props> = ({
             ? { type: submit ? "submit" : "button" }
             : {}),
           className: classnames(
-            "flex w-12/12 justify-center items-center text-center text-white leading-xs transition duration-200 cursor-pointer font-bold text-base focus:outline-none",
+            "flex w-12/12 justify-center items-center text-center text-black leading-xs transition duration-200 cursor-pointer font-bold text-base focus:outline-none",
             {
               [buttonClasses]: !isWrapped,
             }
