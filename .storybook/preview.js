ad7daf49b253c4bb301fbb976b0faae7aebfaf8b
GutierrezGo <virginia@carforyou.ch>
2022-01-04 17:38:30 +0100

    chore: add config for the breakpoints

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,2 +1,29 @@
-import "./stories.css"
-import "../assets/index.css"
+require("../assets/index.css")
+require("./stories.css")
+
+import { addParameters } from '@storybook/react';
+
+const screens = {
+  xxs: '375px',
+  xs: '480px',
+  sm: '576px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  xxl: '1920px'
+}
+
+const customViewports = Object.entries(screens).reduce((acc, [key, value]) => {
+  acc[key] = {
+    name: key,
+    styles: {
+      width: value,
+      height: "100%"
+    }
+  }
+  return acc
+}, {})
+
+addParameters({
+  viewport: { viewports: customViewports },
+})
