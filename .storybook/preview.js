1ac9000e692eca6f07449b25950f853f4d4d7b0e
Virginia <37380787+GutierrezGo@users.noreply.github.com>
2022-01-05 17:53:41 +0100

    chore: add viewport addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,2 +1,21 @@
-import "./stories.css"
-import "../assets/index.css"
+require("../assets/index.css")
+require("./stories.css")
+
+const tailwindConfig = require("../.tailwind/defaultConfig").default
+
+import { addParameters } from '@storybook/react';
+
+const customViewports = Object.entries(tailwindConfig.theme.screens).reduce((acc, [key, value]) => {
+  acc[key] = {
+    name: key,
+    styles: {
+      width: value,
+      height: "100%"
+    }
+  }
+  return acc
+}, {})
+
+addParameters({
+  viewport: { viewports: customViewports },
+})
